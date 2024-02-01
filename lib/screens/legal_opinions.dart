5fdd7dadea1d77642d755dd1ed740ab3e7c79b59
Acelenteria <angelacecilialenteria99@gmail.com>
2024-02-01 16:27:59 +0800

    cardsUpdate

--- lib/screens/legal_opinions.dart
+++ lib/screens/legal_opinions.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import '../screens/sidebar.dart';
+import '../screens/details_screen.dart';
 
-class LegalOpinions extends StatelessWidget {
+class LegalOpinions extends StatefulWidget {
+  @override
+  _LegalOpinionsState createState() => _LegalOpinionsState();
+}
+
+class _LegalOpinionsState extends State<LegalOpinions> {
+  List<String> categories = ['Category 1', 'Category 2', 'Category 3'];
+  String selectedCategory = 'Category 1'; // Default selection
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,15 +28,158 @@ class LegalOpinions extends StatelessWidget {
           ),
         ),
       ),
-      body: Center(
-        child: Text('Content of Legal Opinions'),
-      ),
+      body: _buildBody(),
       drawer: Sidebar(
-        currentIndex: 7, // Adjust the index based on your sidebar menu
+        currentIndex: 7,
         onItemSelected: (index) {
-          // Handle item selection if needed
-          Navigator.pop(context); // Close the drawer after handling selection
+          Navigator.pop(context);
         },
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    TextEditingController searchController = TextEditingController();
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          // Search Input
+          Container(
+            margin: EdgeInsets.only(bottom: 8.0),
+            padding: EdgeInsets.all(12.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey[400]!),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: TextField(
+              controller: searchController,
+              decoration: InputDecoration(
+                hintText: 'Search...',
+                border: InputBorder.none,
+                prefixIcon: Icon(Icons.search),
+              ),
+              onChanged: (value) {
+                // Handle search input changes
+              },
+            ),
+          ),
+
+          // Category Dropdown
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Title/Subject: ',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.grey[400]!),
+                  ),
+                  padding: EdgeInsets.all(12.0),
+                  child: DropdownButton<String>(
+                    value: selectedCategory,
+                    onChanged: (String? newValue) {
+                      if (newValue != null) {
+                        setState(() {
+                          selectedCategory = newValue;
+                        });
+                      }
+                    },
+                    items: categories
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          SizedBox(height: 16.0),
+
+          // Sample Table Section
+          Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Latest',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                for (int index = 0; index < 11; index++)
+                  InkWell(
+                    onTap: () {
+                      _navigateToDetailsPage(context, 'Row $index');
+                    },
+                    child: Card(
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          children: [
+                            Icon(Icons.article,
+                                color: Colors.blue[
+                                    900]), // Replace with your desired icon
+                            SizedBox(width: 16.0),
+                            Expanded(
+                              child: Text(
+                                'Row $index',
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16.0),
+                            Text(
+                              'Date $index',
+                              style: TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _navigateToDetailsPage(BuildContext context, String content) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => DetailsScreen(
+          title: 'Details',
+          content: content,
+        ),
       ),
     );
   }
